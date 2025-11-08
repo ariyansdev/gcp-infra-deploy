facca1d105c3c336cbe8c39aae83310d5383ba36
Subash Subramaniyan <93960578+subash-subramaniyan@users.noreply.github.com>
2025-11-08 09:32:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ variable "region" {
 }
 
 resource "google_storage_bucket" "demo" {
-  name     = "demo-bucket-${var.project_id}"
+  name     = "ariyansappdev-${var.project_id}"
   location = upper(var.region)
   uniform_bucket_level_access = true
 }
